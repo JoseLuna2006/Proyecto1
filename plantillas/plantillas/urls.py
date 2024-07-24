992cebd24dd0a951540b145bf11b16d9da157169
Jose Luna <josemanuelluna2018@gmail.com>
2024-07-23 19:58:05 -0500

    Se implementó el uso de contexto en la plantilla

--- plantillas/plantillas/urls.py
+++ plantillas/plantillas/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("simple/", views.simple, name="simple")
-
+    path("simple/", views.simple, name="simple"),
+    path("dinamico/<str:name>/", views.dinamico, name="dinamico")
 ]
